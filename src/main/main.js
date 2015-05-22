c677d674a68fb8bb66be6cc1a03c2daed3398bb3
sandeshds <sandeshdoolipeta@gmail.com>
2015-05-22 20:08:08 +0530

    Adding material design

--- src/main/main.js
+++ src/main/main.js
@@ -1,23 +1,10 @@
 
-function generatePlayer() {
-    d3.select("body").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
+function newNote() {
+	document.getElementById("form").style.visibility = "visible";
+	
 };
 
-function generateCircle() {
-	generatePlayer();
-	d3.select("body").append("br");
-    d3.select("body").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "green")
-    .transition()
-    .delay(100)
-    .duration(1000)    
-    .attr("r", 10)
-    .attr("cx", 30)
-    .style("fill", "black")
-    .transition()
-    .delay(1000)
-    .duration(1000)    
-    .attr("r", 100)
-    .attr("cx", 30)
-    .attr("cy", 30)
-    .style("fill", "blue");
-};
+function addNote() {
+	var title = document.getElementById('title').value;
+	console.log(title);
+}
